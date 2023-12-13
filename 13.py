6f8b61b54703de5086962d74facdf55e61444597
Julien CHATRY <chatryjulien@gmail.com>
2023-12-13 18:15:23 +0100

    Cleaned Day 13 (again)

--- 13.py
+++ 13.py
@@ -6,45 +6,34 @@ def get_txt_array(input: str) -> List[str]:
     with open(input,'r',encoding = 'utf-8') as f:
         return f.read().splitlines()
         
-def check_mirror(t: List[List[str]], axis: str, i: int, verbose = False, tolerance = 0)-> bool:
-    #mirror line between i and i+1
-    
-    d = 0 #differences counter
-    
-    if axis == "h": #horizontal
-        y = min(len(t)-i-1,i+1) #distance to closest border
+def compute_strings(t: List[List[str]], axis: str, i: int, dx: int) -> tuple:
+    if axis == "h":  # Horizontal
+        s1 = t[i + dx + 1]
+        s2 = t[i - dx]
+    else:  # Vertical
+        s1 = "".join(line[i + dx + 1] for line in t)
+        s2 = "".join(line[i - dx] for line in t)
+    return s1, s2
         
-        for dy in range(y):
-            s1 = t[i+dy+1]
-            s2 = t[i-dy]
-            
-            if verbose:
-                print(i+dy+1, i-dy)
-                print(f"Currently comparing these two :\n{s1}\n{s2}")
-                
-            if s1 != s2:
-                differing_count = sum(1 for a, b in zip(s1, s2) if a != b)
-                d += differing_count
-                
-    else: #vertical
-        x = min(len(t[0])-i-1,i+1) #distance to closest border
-        
-        for dx in range(x):
-            s1 = "".join([line[i+dx+1] for line in t])
-            s2 = "".join([line[i-dx] for line in t])
-            
-            if verbose:
-                print(i+dx+1, i-dx)
-                print(f"Currently comparing these two :\n{s1}\n{s2}")
-                
-            if s1 != s2:
-                differing_count = sum(1 for a, b in zip(s1, s2) if a != b)
-                d += differing_count
+def check_mirror(t: List[List[str]], axis: str, i: int, verbose=False, tolerance=0) -> bool:
+    # Mirror line between i and i+1
     
+    d = 0  # Differences counter
+    n = min(len(t) - i - 1, i + 1) if axis == "h" else min(len(t[0]) - i - 1, i + 1)  # Distance to closest border
+    
+    for dn in range(n):
+        s1, s2 = compute_strings(t, axis, i, dn)
+
+        if verbose:
+            print(f"{i + dn + 1}, {i - dn}")
+            print(f"Currently comparing these two:\n{s1}\n{s2}")
+
+        differing_count = sum(1 for a, b in zip(s1, s2) if a != b)
+        d += differing_count
+
     return d == tolerance
         
 def prettyprint(a,b,c):
-    print(b)
     if c == "h":
         for i, line in enumerate(a):
             if i == b :
@@ -63,12 +52,13 @@ def prettyprint(a,b,c):
         for k in range(len(a[0])):
             print((k%10), end = "")
 
-def solve(input: str, part: int, debug = False)-> int:
+def solve(input: str, part: int, debug = False) -> int:
     txt = get_txt_array(input)
     formated_txt = [[]]
     result = 0
-    tol = 0 if part == 1 else 1
+    tol = 1 if part == 2 else 0
     
+    #Separate each dataset
     for i in range(len(txt)):
         if not txt[i]:
             formated_txt.append([])
@@ -78,7 +68,7 @@ def solve(input: str, part: int, debug = False)-> int:
     for dataset in formated_txt:
         height = len(dataset)
         width = len(dataset[0])
-        mirror = 0
+        mirror = -1
         
         for y in range(0,height-1):
             if check_mirror(dataset, "h", y, tolerance = tol):
@@ -86,7 +76,7 @@ def solve(input: str, part: int, debug = False)-> int:
                 mirror = y+1
                 mirror_type = "h"
                 
-                
+        
         for x in range(0,width-1):
             if check_mirror(dataset, "v", x, tolerance = tol):
                 result += (x+1)
@@ -99,5 +89,5 @@ def solve(input: str, part: int, debug = False)-> int:
     
     return result
 
-a = solve("13-input.txt", part = 1, debug = False)
+a = solve("13-input.txt", part = 2, debug = True)
 print(a)
