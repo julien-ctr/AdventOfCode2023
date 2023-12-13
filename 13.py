7681cc53e52793b47a03c51b2c9d2d2a3acca1a0
Julien CHATRY <chatryjulien@gmail.com>
2023-12-13 17:50:55 +0100

    Refactored day 13
    merged some functions to avoid code redundancy

--- 13.py
+++ 13.py
@@ -5,9 +5,11 @@ from typing import *
 def get_txt_array(input: str) -> List[str]:
     with open(input,'r',encoding = 'utf-8') as f:
         return f.read().splitlines()
-
-def check_mirror(t: List[List[str]], axis: str, i: int, verbose = False)-> bool:
+        
+def check_mirror(t: List[List[str]], axis: str, i: int, verbose = False, tolerance = 0)-> bool:
     #mirror line between i and i+1
+    
+    d = 0 #differences counter
     
     if axis == "h": #horizontal
         y = min(len(t)-i-1,i+1) #distance to closest border
@@ -18,50 +20,13 @@ def check_mirror(t: List[List[str]], axis: str, i: int, verbose = False)-> bool:
             
             if verbose:
                 print(i+dy+1, i-dy)
-                print(f"CurrentlY comparing these two :\n{s1}\n{s2}")
-                
-            if s1 != s2:
-                return False
-                
-        return True
-    
-    if axis == "v":
-        x = min(len(t[0])-i-1,i+1) #distance to closest border
-        
-        for dx in range(x):
-            s1 = "".join([line[i+dx+1] for line in t])
-            s2 = "".join([line[i-dx] for line in t])
-            
-            if verbose:
-                print(i+dx+1, i-dx)
                 print(f"Currently comparing these two :\n{s1}\n{s2}")
-                
-            if s1 != s2:
-                return False
-                
-        return True
-        
-def check_almost_mirror(t: List[List[str]], axis: str, i: int, verbose = False)-> bool:
-    #mirror line between i and i+1
-    d = 0
-    if axis == "h": #horizontal
-        y = min(len(t)-i-1,i+1) #distance to closest border
-        
-        for dy in range(y):
-            s1 = t[i+dy+1]
-            s2 = t[i-dy]
-            
-            if verbose:
-                print(i+dy+1, i-dy)
-                print(f"CurrentlY comparing these two :\n{s1}\n{s2}")
                 
             if s1 != s2:
                 differing_count = sum(1 for a, b in zip(s1, s2) if a != b)
                 d += differing_count
                 
-        
-    
-    if axis == "v":
+    else: #vertical
         x = min(len(t[0])-i-1,i+1) #distance to closest border
         
         for dx in range(x):
@@ -76,7 +41,7 @@ def check_almost_mirror(t: List[List[str]], axis: str, i: int, verbose = False)-
                 differing_count = sum(1 for a, b in zip(s1, s2) if a != b)
                 d += differing_count
     
-    return d == 1
+    return d == tolerance
         
 def prettyprint(a,b,c):
     print(b)
@@ -98,10 +63,11 @@ def prettyprint(a,b,c):
         for k in range(len(a[0])):
             print((k%10), end = "")
 
-def part1(input: str, debug = False)-> int:
+def solve(input: str, part: int, debug = False)-> int:
     txt = get_txt_array(input)
     formated_txt = [[]]
     result = 0
+    tol = 0 if part == 1 else 1
     
     for i in range(len(txt)):
         if not txt[i]:
@@ -115,14 +81,14 @@ def part1(input: str, debug = False)-> int:
         mirror = 0
         
         for y in range(0,height-1):
-            if check_mirror(dataset,"h",y):
+            if check_mirror(dataset, "h", y, tolerance = tol):
                 result += 100*(y+1)
                 mirror = y+1
                 mirror_type = "h"
                 
                 
         for x in range(0,width-1):
-            if check_mirror(dataset,"v",x):
+            if check_mirror(dataset, "v", x, tolerance = tol):
                 result += (x+1)
                 mirror = x+1
                 mirror_type = "v"
@@ -130,43 +96,8 @@ def part1(input: str, debug = False)-> int:
         if debug:
             print("\n\n[New dataset]")
             prettyprint(dataset, mirror-1, mirror_type)
-                
+    
     return result
 
-def part2(input: str, debug = False)-> int:
-    txt = get_txt_array(input)
-    formated_txt = [[]]
-    result = 0
-    
-    for i in range(len(txt)):
-        if not txt[i]:
-            formated_txt.append([])
-        else:
-            formated_txt[-1].append(txt[i])
-    
-    for dataset in formated_txt:
-        height = len(dataset)
-        width = len(dataset[0])
-        mirror = 0
-        
-        for y in range(0,height-1):
-            if check_almost_mirror(dataset,"h",y):
-                result += 100*(y+1)
-                mirror = y+1
-                mirror_type = "h"
-                
-                
-        for x in range(0,width-1):
-            if check_almost_mirror(dataset,"v",x):
-                result += (x+1)
-                mirror = x+1
-                mirror_type = "v"
-                
-        if debug:
-            print("\n\n[New dataset]")
-            prettyprint(dataset, mirror-1, mirror_type)
-                
-    print(result)
-    return result
-    
-part2("13-input.txt", debug = True)
+a = solve("13-input.txt", part = 1, debug = False)
+print(a)
